591a69bf5b809696f0633f298037ad490205ac8c
Dhruv2311 <dhruv.sonagra.cg@gmail.com>
2026-03-14 11:06:34 +0530

    feat(server): add rest endpoints for stock transfer

--- server/routes/stock.routes.js
+++ server/routes/stock.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getStock, updateStock, getStockMoves } from '../controllers/stock.controller.js';
+import { getStock, updateStock, getStockMoves, transferStock } from '../controllers/stock.controller.js';
 
 const router = express.Router();
 
 router.get('/', getStock);
 router.put('/update', updateStock);
 router.get('/moves', getStockMoves);
+router.post('/transfer', transferStock);
 
 export default router;

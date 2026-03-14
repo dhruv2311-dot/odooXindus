bec73b3758f098c11a3904b0c1f1f03e43a6250e
shubhamiscodding <shubham.modi.cg@gmail.com>
2026-03-14 14:54:26 +0530

    feat(server-routes): add receipt status update route

--- server/routes/receipt.routes.js
+++ server/routes/receipt.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getReceipts, createReceipt, getReceiptById, validateReceipt } from '../controllers/receipt.controller.js';
+import { getReceipts, createReceipt, getReceiptById, validateReceipt, updateReceiptStatus } from '../controllers/receipt.controller.js';
 
 const router = express.Router();
 
 router.get('/', getReceipts);
 router.post('/', createReceipt);
 router.get('/:id', getReceiptById);
+router.put('/:id/status', updateReceiptStatus);
 router.put('/:id/validate', validateReceipt);
 
 export default router;
